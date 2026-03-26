791b7008994d873753a795c51db459452b5459c5
Light <danil.odinzov181@gmail.com>
2026-03-26 20:56:55 +0500

    Изменение:
    - Изменение сигнатуры методов IFileUseCase
    -- Добавлен контекст во все методы
    -- PipeReader в GetFile заменен на ReadCloser для унификации

--- internal/interfaces/file_use_case.go
+++ internal/interfaces/file_use_case.go
@@ -1,14 +1,15 @@
 package interfaces
 
 import (
+	"context"
 	"insync/internal/domain"
 	"io"
 )
 
 type IFileUseCase interface {
-	GetFileList(rootName string) ([]domain.FileMetadata, error)
-	DeleteFile(rootName, relativePath string) error
-	PutFile(rootName, relativePath string, file io.Reader) error
-	RenameFile(rootName, oldRelativePath, newRelativePath string) error
-	GetFile(rootName, relativePath string) (*io.PipeReader, error)
+	GetFileList(ctx context.Context, rootName string) ([]domain.FileMetadata, error)
+	DeleteFile(ctx context.Context, rootName, relativePath string) error
+	PutFile(ctx context.Context, rootName, relativePath string, file io.Reader) error
+	RenameFile(ctx context.Context, rootName, oldRelativePath, newRelativePath string) error
+	GetFile(ctx context.Context, rootName, relativePath string) (*io.ReadCloser, error)
 }
