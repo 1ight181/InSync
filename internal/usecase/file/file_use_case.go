290a4372b2ac7c13dca346f98c72042d71585f1e
Light <danil.odinzov181@gmail.com>
2026-04-07 21:42:38 +0500

    Добавление функционала:
    - Реализованы методы у FileUseCase

--- internal/usecase/file/file_use_case.go
+++ internal/usecase/file/file_use_case.go
@@ -20,21 +20,21 @@ func NewFileUseCase(opts FileUseCaseOptions) ifaces.IFileUseCase {
 }
 
 func (f *FileUseCase) GetFileList(ctx context.Context, rootName domain.RootName) ([]domain.FileEntry, error) {
-	return nil, nil
+	return f.fileManager.GetFileList(ctx, rootName)
 }
 
 func (f *FileUseCase) DeleteFile(ctx context.Context, rootName domain.RootName, relativePath domain.RelativePath) error {
-	return nil
+	return f.fileManager.DeleteFile(ctx, rootName, relativePath)
 }
 
 func (f *FileUseCase) PutFile(ctx context.Context, rootName domain.RootName, relativePath domain.RelativePath, file io.Reader) error {
-	return nil
+	return f.fileManager.PutFile(ctx, rootName, relativePath, file)
 }
 
 func (f *FileUseCase) RenameFile(ctx context.Context, rootName domain.RootName, oldRelativePath, newRelativePath domain.RelativePath) error {
-	return nil
+	return f.fileManager.RenameFile(ctx, rootName, oldRelativePath, newRelativePath)
 }
 
 func (f *FileUseCase) GetFile(ctx context.Context, rootName domain.RootName, relativePath domain.RelativePath) (io.ReadCloser, error) {
-	return nil, nil
+	return f.fileManager.GetFile(ctx, rootName, relativePath)
 }
