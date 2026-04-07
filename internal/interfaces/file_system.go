f2aac4691332ce000abe7f899d76e8562145804b
Light <danil.odinzov181@gmail.com>
2026-04-07 21:28:46 +0500

    Изменение:
    - Изменена сигнатура методов в IFileSystem

--- internal/interfaces/file_system.go
+++ internal/interfaces/file_system.go
@@ -8,10 +8,10 @@ import (
 type IFileSystem interface {
 	ReadDir(fullPath string) ([]fs.DirEntry, error)
 	WalkDir(fullPath string, walkFn func(path string, d fs.DirEntry, err error) error) error
-	Open(fullPath string) (io.ReadCloser, error)
+	Open(fullPath string) (fs.File, error)
 	Remove(fullPath string) error
-	CreateTempFile(dir, pattern string) (io.WriteCloser, string, error)
+	CreateTempFile(dir, pattern string) (io.ReadWriteCloser, string, error)
 	Rename(oldPath, newPath string) error
 	MkdirAll(path string, perm fs.FileMode) error
-	Create(fullPath string) (io.WriteCloser, error)
+	Create(fullPath string) (io.ReadWriteCloser, error)
 }
