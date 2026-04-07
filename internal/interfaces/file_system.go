bebd27cf69e9e6394c87e25180db3b0c3789d158
Light <danil.odinzov181@gmail.com>
2026-04-07 16:37:19 +0500

    Добавление функционала:
    - Расширен интерфейс IFileSystem

--- internal/interfaces/file_system.go
+++ internal/interfaces/file_system.go
@@ -9,4 +9,5 @@ type IFileSystem interface {
 	ReadDir(fullPath string) ([]fs.DirEntry, error)
 	WalkDir(fullPath string, walkFn func(path string, d fs.DirEntry, err error) error) error
 	Open(fullPath string) (io.ReadCloser, error)
+	Remove(fullPath string) error
 }
