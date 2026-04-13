0c79d9efec76e9ec036d1c1426ac91450d13d7b1
Light <danil.odinzov181@gmail.com>
2026-04-13 16:20:24 +0500

    Изменение:
    - Контракт IRootResolver для FileManager сокращен ввиду избыточности

--- internal/infrastructure/filemanager/root_resolver.go
+++ internal/infrastructure/filemanager/root_resolver.go
@@ -2,6 +2,4 @@ package filemanager
 
 type IRootResolver interface {
 	ResolveRoot(rootName string, relativePath string) (string, error)
-	AddRoot(rootName string, rootPath string)
-	RemoveRoot(rootName string)
 }
