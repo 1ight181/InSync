c8cb2ea5310a5d2aec647c6bead06bc6049385e4
Light <danil.odinzov181@gmail.com>
2026-04-16 15:46:53 +0500

    Добавление функционала:
    - Реализован LocalSnapshotProvider

---
+++ internal/infrastructure/planresolver/local/file_manager.go
@@ -0,0 +1,10 @@
+package local
+
+import (
+	"context"
+	"insync/internal/domain"
+)
+
+type IFileManager interface {
+	GetSnapshot(ctx context.Context, rootName domain.RootName) (domain.Snapshot, error)
+}
---
+++ internal/infrastructure/planresolver/local/local_snapshot_provider.go
@@ -0,0 +1,25 @@
+package local
+
+import (
+	"context"
+	"insync/internal/domain"
+)
+
+type LocalSnapshotProvider struct {
+	fileManager IFileManager
+}
+
+type LocalSnapshotProviderOptions struct {
+	FileManager IFileManager
+}
+
+func NewLocalSnapshotProvider(opts LocalSnapshotProviderOptions) *LocalSnapshotProvider {
+	if opts.FileManager == nil {
+		panic("Все поля LocalSnapshotProviderOptions должны быть заполнены")
+	}
+	return &LocalSnapshotProvider{fileManager: opts.FileManager}
+}
+
+func (p *LocalSnapshotProvider) GetSnapshot(ctx context.Context, rootName domain.RootName) (domain.Snapshot, error) {
+	return p.fileManager.GetSnapshot(ctx, rootName)
+}
