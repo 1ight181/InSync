3921a337788eae46984fce9c340ed7ffa577e5b2
Light <danil.odinzov181@gmail.com>
2026-04-27 14:42:22 +0500

    Рефакторинг:
    - Убрана неиспользуемая модель конфига RemoteDeviceIdResolverConfig

--- internal/infrastructure/config/models/config.go
+++ internal/infrastructure/config/models/config.go
@@ -2,14 +2,13 @@ package models
 
 // Реализует интерфейс ConfigModel
 type GeneralConfig struct {
-	DbConfig                     DbConfig                     `mapstructure:"db"`
-	ServerConfig                 ServerConfig                 `mapstructure:"server"`
-	ClientConfig                 ClientConfig                 `mapstructure:"client"`
-	LoggerConfig                 LoggerConfig                 `mapstructure:"logger"`
-	MDnsServerConfig             MDnsServerConfig             `mapstructure:"mdns_server"`
-	MDnsBrowserConfig            MDnsBrowserConfig            `mapstructure:"mdns_browser"`
-	RemoteDeviceIdResolverConfig RemoteDeviceIdResolverConfig `mapstructure:"remote_device_id_resolver"`
-	DeviceIdResolverConfig       DeviceIdResolverConfig       `mapstructure:"device_id_resolver"`
+	DbConfig               DbConfig               `mapstructure:"db"`
+	ServerConfig           ServerConfig           `mapstructure:"server"`
+	ClientConfig           ClientConfig           `mapstructure:"client"`
+	LoggerConfig           LoggerConfig           `mapstructure:"logger"`
+	MDnsServerConfig       MDnsServerConfig       `mapstructure:"mdns_server"`
+	MDnsBrowserConfig      MDnsBrowserConfig      `mapstructure:"mdns_browser"`
+	DeviceIdResolverConfig DeviceIdResolverConfig `mapstructure:"device_id_resolver"`
 }
 
 func (gc *GeneralConfig) Validate() error {
@@ -29,9 +28,6 @@ func (gc *GeneralConfig) Validate() error {
 		return err
 	}
 	if err := gc.MDnsBrowserConfig.Validate(); err != nil {
-		return err
-	}
-	if err := gc.RemoteDeviceIdResolverConfig.Validate(); err != nil {
 		return err
 	}
 
